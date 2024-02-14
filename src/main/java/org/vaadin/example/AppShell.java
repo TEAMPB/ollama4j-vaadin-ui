75967f94977c5034266cdc9df57d7ff230a0fbe6
set <set>
2024-02-14 17:20:56 +0000

    Adds ChatView with streamed Chat API call sample

--- src/main/java/org/vaadin/example/AppShell.java
+++ src/main/java/org/vaadin/example/AppShell.java
@@ -1,14 +1,17 @@
 package org.vaadin.example;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 
 /**
  * Use the @PWA annotation make the application installable on phones, tablets
  * and some desktop browsers.
  */
-@PWA(name = "Project Base for Vaadin", shortName = "Project Base")
-@Theme(variant = "dark")
+@PWA(name = "Ollama4j UI", shortName = "Ollama4j UI")
+@Theme(variant = Lumo.DARK)
+@Push
 public class AppShell implements AppShellConfigurator {
 }
