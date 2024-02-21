227d2da71b4a276fee36b068b8c523732835438f
Markus Klenke <markusklenke86@gmail.com>
2024-02-21 22:30:35 +0000

    Fixes null response issues for ModelDetails API call

--- src/main/java/agency/schmecker/dev/ollama4j/ui/view/model/ListModelsView.java
+++ src/main/java/agency/schmecker/dev/ollama4j/ui/view/model/ListModelsView.java
@@ -29,20 +29,20 @@ import io.github.amithkoujalgi.ollama4j.core.models.ModelDetail;
 import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
 
-@Route(value = "model/list",layout = MainLayout.class)
+@Route(value = "model/list", layout = MainLayout.class)
 @PageTitle("List loaded models")
 @CdiComponent
-public class ListModelsView extends VerticalLayout{
+public class ListModelsView extends VerticalLayout {
 
     private static final Logger LOG = LoggerFactory.getLogger(ListModelsView.class);
 
-    private Grid<Model> grid = new Grid<>(Model.class,false);
+    private Grid<Model> grid = new Grid<>(Model.class, false);
 
     @Inject
     private ModelService modelService;
 
     @PostConstruct
-    public void init(){
+    public void init() {
         H2 header = new H2("Loaded models");
         grid.addColumn(Model::getModelName).setHeader("Model Name");
         grid.addColumn(Model::getModelVersion).setHeader("Model Version");
@@ -58,43 +58,43 @@ public class ListModelsView extends VerticalLayout{
         this.setHeightFull();
     }
 
-    private void refreshGrid(){
-        LOG.info("Refreshing Grid: " +  grid);
+    private void refreshGrid() {
+        LOG.info("Refreshing Grid: " + grid);
         modelService.refreshLoadedModels();
         grid.setItems(modelService.getLoadedModels());
     }
 
-    private ComponentRenderer<ModelDeleteButton,Model> createDeleteButtonRenderer(){
-        return new ComponentRenderer<>(ModelDeleteButton::new,ModelDeleteButton::setModel);
+    private ComponentRenderer<ModelDeleteButton, Model> createDeleteButtonRenderer() {
+        return new ComponentRenderer<>(ModelDeleteButton::new, ModelDeleteButton::setModel);
     }
 
-    private ComponentRenderer<ModelDetailsFormLayout, Model> createModelDetailsRenderer(){
-        return new ComponentRenderer<>(ModelDetailsFormLayout::new,ModelDetailsFormLayout::setModel);
+    private ComponentRenderer<ModelDetailsFormLayout, Model> createModelDetailsRenderer() {
+        return new ComponentRenderer<>(ModelDetailsFormLayout::new,
+                ModelDetailsFormLayout::setModel);
     }
 
     private class ModelDeleteButton extends Button {
 
         private Model m;
 
-        public ModelDeleteButton(){
+        public ModelDeleteButton() {
             this.setIcon(new Icon(VaadinIcon.TRASH));
             this.addThemeVariants(ButtonVariant.LUMO_ERROR);
-            this.addClickListener((e)->{
+            this.addClickListener((e) -> {
                 ConfirmDialog dialog = new ConfirmDialog();
                 dialog.setHeader("Delete model");
-                dialog.setText(
-                        "Do you want to remove the model '" + m.getModel() +"'?");
+                dialog.setText("Do you want to remove the model '" + m.getModel() + "'?");
                 dialog.setRejectable(true);
                 Button rejectButton = new Button(new Icon(VaadinIcon.THUMBS_DOWN));
                 dialog.setRejectButton(rejectButton);
 
                 Button confirmButton = new Button(new Icon(VaadinIcon.THUMBS_UP));
-                confirmButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY,ButtonVariant.LUMO_ERROR);
+                confirmButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY,
+                        ButtonVariant.LUMO_ERROR);
                 dialog.setConfirmButton(confirmButton);
                 dialog.addConfirmListener(event -> {
                     modelService.deleteModel(m.getModel());
-                    Notification notification = Notification
-                        .show("Model deleted!");
+                    Notification notification = Notification.show("Model deleted!");
                     notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
                     refreshGrid();
                 });
@@ -102,7 +102,7 @@ public class ListModelsView extends VerticalLayout{
             });
         }
 
-        public void setModel(Model m){
+        public void setModel(Model m) {
             this.m = m;
         }
     }
@@ -114,21 +114,34 @@ public class ListModelsView extends VerticalLayout{
         private final TextArea details = new TextArea("Details");
         private final TextArea license = new TextArea("License");
 
-        public ModelDetailsFormLayout(){
-                Stream.of(modelFile, parameters, template, details, license).forEach(field -> {
-                            field.setReadOnly(true);
-                            add(field);
-                        });
-            }
+        public ModelDetailsFormLayout() {
+            Stream.of(modelFile, parameters, template, details, license).forEach(field -> {
+                field.setReadOnly(true);
+                add(field);
+            });
+        }
 
         public void setModel(Model model) {
             ModelDetail modelDetail = modelService.loadModelDetail(model);
-            modelFile.setValue(modelDetail.getModelFile());
-            parameters.setValue(modelDetail.getParameters());
-            template.setValue(modelDetail.getTemplate());
-            details.setValue(modelDetail.getDetails().toString());
-            license.setValue(modelDetail.getLicense());
+            if (modelDetail == null) {
+                return;
+            }
+            if (modelDetail.getModelFile() != null) {
+                modelFile.setValue(modelDetail.getModelFile());
+            }
+            if (modelDetail.getParameters() != null) {
+                parameters.setValue(modelDetail.getParameters());
+            }
+            if (modelDetail.getTemplate() != null) {
+                template.setValue(modelDetail.getTemplate());
+            }
+            if (modelDetail.getDetails() != null) {
+                details.setValue(modelDetail.getDetails().toString());
+            }
+            if (modelDetail.getLicense() != null) {
+                license.setValue(modelDetail.getLicense());
+            }
         }
     }
-    
+
 }
